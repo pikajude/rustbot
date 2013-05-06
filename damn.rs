a2be8b18f9ded02cccd9f4a556c7244d8f69ee7d
Joel <joel@otte.rs>
2013-05-06 11:55:43 -0700

    this SHOULD work hehehe

--- damn.rs
+++ damn.rs
@@ -8,10 +8,9 @@ struct Damn {
 }
 
 pub fn make_damn() -> result::Result<~Damn, TcpConnectErrData> {
-  let curTask = &std::uv::global_loop::get();
-  let ips = unwrap(std::net_ip::get_addr("chat.deviantart.com", curTask));
-  match connect(copy ips[0], 3900, curTask) {
-    Ok(s) => Ok(~Damn { sock: socket_buf(s) }),
-    Err(v) => Err(v)
+  let curTask = &std::uv::global_loop::get(),
+      ips = unwrap(std::net_ip::get_addr("chat.deviantart.com", curTask));
+  do map(&connect(copy ips[0], 3900, curTask)) |&sk| {
+    ~Damn { sock: socket_buf(sk) }
   }
 }
